a3107770d1209127bcb7cb354fa877c20f12dce7
Vsevolod245 <87520858+Vsevolod245@users.noreply.github.com>
2021-07-16 18:44:44 +0300

    Удаление старого класса для невязок Создание нового класса для невязок Создан класс TPointSystemResidualDifferencial с наследованием от TPointSystemDifferencial Частично поправлен файл TSystemDifferentialSolverWithResidual.cs

---
+++ PL_1/DifferentialEquations/TResidual.cs
@@ -0,0 +1,21 @@
+﻿//Class which describe main attributes of residual, such as Name and Value of Residual. Value of residual determines for each step.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandartHelperLibrary.MathHelper
+{
+    public class TResidual
+    {
+        /// <summary>
+        /// Name of residual
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Value of residual
+        /// </summary>
+        public double Value { get; set; }
+    }
+}
